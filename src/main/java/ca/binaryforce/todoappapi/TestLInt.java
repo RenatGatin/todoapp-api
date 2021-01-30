629c98f76ab5a145bdd15750a53287ffc30a5870
Renat Gatin <rgatin@trustarc.com>
2021-01-29 23:46:19 -0800

    ATHENA-50 Test Linter 1.

---
+++ src/main/java/ca/binaryforce/todoappapi/TestLInt.java
@@ -0,0 +1,13 @@
+package ca.binaryforce.todoappapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestLInt {
+    private   String   abc;
+    private  boolean sdlfjaslfkj;
+}
