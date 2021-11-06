2f35ed5ce0150629b57166f2046b5a5ba7751e79
Renat Gatin <rgatin@trustarc.com>
2021-11-06 02:19:56 -0700

    ATHENA-56 Test Deleting untagged images on AWS ECR.

--- src/main/java/ca/binaryforce/todoappapi/TestController.java
+++ src/main/java/ca/binaryforce/todoappapi/TestController.java
@@ -13,6 +13,6 @@ public class TestController {
      */
     @RequestMapping("/hello")
     public String sayHello() {
-        return "Hello Binary World! 5";
+        return "Hello Binary World! Test Delete <untagged>";
     }
 }
