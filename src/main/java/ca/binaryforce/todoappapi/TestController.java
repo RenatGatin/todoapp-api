e54719291273c691ccf138f821a7dbe5a133c21e
Renat Gatin <rgatin@trustarc.com>
2021-11-05 23:18:21 -0700

    ATHENA-56 Test ECR Update -> ECS 33.

--- src/main/java/ca/binaryforce/todoappapi/TestController.java
+++ src/main/java/ca/binaryforce/todoappapi/TestController.java
@@ -13,6 +13,6 @@ public class TestController {
      */
     @RequestMapping("/hello")
     public String sayHello() {
-        return "Hello Binary World! 2";
+        return "Hello Binary World! 3";
     }
 }
